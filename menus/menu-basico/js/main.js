6310e3f1579049b18593e5b94e89c98d1e284ed6
jose esteves <josefelixestevenpacheco@gmail.com>
2023-02-23 20:07:44 -0400

    cambi de estylos y mejora en el js

--- menus/menu-basico/js/main.js
+++ menus/menu-basico/js/main.js
@@ -8,11 +8,11 @@ document.addEventListener('click', e => {
 		items.forEach(i => {
 			i.classList.toggle('menu__item--show');
 		});
+		e.target.firstElementChild.classList.add('btnMenu__iconBar--bg')
 		e.target.disabled = true;
-		items[items.length - 1].addEventListener(
-			'transitionend',
-			e => {
-				e.target.offsetParent.parentElement.parentElement.firstElementChild.disabled = false;
+		items[items.length - 1].addEventListener('transitionend',e => {
+			e.target.offsetParent.parentElement.parentElement.firstElementChild.disabled = false;
+			e.target.offsetParent.parentElement.parentElement.firstElementChild.firstElementChild.classList.remove('btnMenu__iconBar--bg')
 			},
 			{ once: true }
 		);
