f05d3188b7a7750b1301350eaa2254d947ddc47f
José Esteves <joseesteves199930@gmail.com>
2023-04-05 19:01:59 -0400

    formulario actualizado validacion de campos

--- formulario-buson-sugerencia-email/js/main.js
+++ formulario-buson-sugerencia-email/js/main.js
@@ -6,20 +6,17 @@ const validate = ({ input, reEx, e, msg, name }) => {
 		e.target.classList.add('form__input--correct');
 		e.target.classList.remove('form__input--error');
 		e.target.nextElementSibling.classList.remove('form__inputMsg--show');
-		compos[name] = true;
 	} else {
 		e.target.classList.remove('form__input--correct');
 		e.target.classList.add('form__input--error');
 		e.target.nextElementSibling.textContent = msg;
 		e.target.nextElementSibling.classList.add('form__inputMsg--show');
-		compos[name] = false;
 	}
 	if (input === '') {
 		e.target.classList.add('form__input--error');
 		e.target.nextElementSibling.classList.add('form__inputMsg--show');
 		e.target.nextElementSibling.textContent =
 			'Este campo no puede ir vacion es requerido';
-		compos[name] = false;
 	}
 };
 const reEx = {
@@ -36,17 +33,15 @@ const msg = {
 	comments:
 		'El campo "Comentario" se compone de minimo 10 y maximo 255 caracteres ',
 };
-const compos = {
-	name: false,
-	email: false,
-	subject: false,
-	comments: false,
-};
 form.addEventListener('submit', e => {
 	e.preventDefault();
-	const { name, email, subject, comments } = compos;
-	if (name && email && subject && comments) {
-		const { name, email, subject, comments } = e.target;
+	const { name, email, subject, comments } = e.target;
+	if (
+		name.value !== '' &&
+		email.value !== '' &&
+		subject.value !== '' &&
+		comments.value !== ''
+	) {
 		const url = 'https://formsubmit.co/ajax/joseesteves199930@gmail.com';
 		const body = {
 			name: name.value,
